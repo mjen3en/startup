3c774d745a0c625991a62f84cda7f65197be46e7
Micah <batmanwillreturn@gmail.com>
2025-03-13 19:04:21 -0600

    implement login/logout endpoints

---
+++ service/index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const app = express();
+
+const cookieParser = require('cookie-parser');
+const bcrypt = require('bcryptjs');
+const uuid = require('uuid');
+app.use(express.json());
+let users = [];
+let scores = [];
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+apiRouter.post('/auth/create', async (req, res) => {
+    if (await findUser('email', req.body.email)) {
+      res.status(409).send({ msg: 'Existing user' });
+    } else {
+      const user = await createUser(req.body.email, req.body.password);
+  
+      setAuthCookie(res, user.token);
+      res.send({ email: user.email });
+    }
+  });
+
+  // GetAuth login an existing user
+apiRouter.post('/auth/login', async (req, res) => {
+    const user = await findUser('email', req.body.email);
+    if (user) {
+      if (await bcrypt.compare(req.body.password, user.password)) {
+        user.token = uuid.v4();
+        setAuthCookie(res, user.token);
+        res.send({ email: user.email });
+        return;
+      }
+    }
+    res.status(401).send({ msg: 'Unauthorized' });
+  });
+  
+  // DeleteAuth logout a user
+  apiRouter.delete('/auth/logout', async (req, res) => {
+    const user = await findUser('token', req.cookies[authCookieName]);
+    if (user) {
+      delete user.token;
+    }
+    res.clearCookie(authCookieName);
+    res.status(204).end();
+  });
+
+  
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+  });
